5eafb8d6c3d70c736764e66326ae997ddfa76b16
Juanjo <elquefaltaba@gmail.com>
2023-05-17 15:25:36 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,123 @@
+import { useState } from 'react'
+import { useCSVReader, useCSVDownloader } from 'react-papaparse'
+
+import './App.css'
+
+function App() {
+  
+  const { CSVReader } = useCSVReader()
+  const { CSVDownloader, Type } = useCSVDownloader()
+  
+  const [paginaPueblosData, setPaginasPueblosData] = useState()
+  const [masVotadosData, setMasVotadosData] = useState()
+  const [dataByParty, setDataByParty] = useState()
+  
+  const handlePaginaPueblos = data => {
+    const municipiosObjects = data.map(row => {
+      return {
+        municipio: row[6]?.trim(),
+        porcentajeVotantes: Number(row[14]),
+        uno: row[23]?.trim(),
+        unoConcejales: Number(row[26]),
+        dos: row[28]?.trim(),
+        dosConcejales: Number(row[31]),
+        tres: row[33]?.trim(),
+        tresConcejales: Number(row[36]),
+        cuatro: row[38]?.trim(),
+        cuatroConcejales: Number(row[41]),
+        cinco: row[43]?.trim(),
+        cincoConcejales: Number(row[46]),
+        seis: row[48]?.trim(),
+        seisConcejales: Number(row[51])
+      }
+    })
+    
+    setPaginasPueblosData(municipiosObjects)
+  }
+  
+  const handleMasVotados = data => {
+    const masVotados = data.map(row => {
+      return {
+        municipio: row[6]?.trim(),
+        masVotado: row[23]?.trim(),
+        porcentajeVoto: Number(row[25]),
+        concejales: Number(row[26])
+      }
+    })
+    
+    setMasVotadosData(masVotados)
+  }
+  
+  const handleOrderedByParty = data => {
+
+    const byParty = []
+    let tempObj = { psoe: null, pp: null, vox: null, cs: null, podemos: null, iu: null, canda: null, pmas: null, axsi: null }
+    
+    data.map(row => {
+
+      if (row.length > 1) {
+
+        for (let columnNumber = 0; columnNumber < row.length; columnNumber++) {
+          if (typeof row[columnNumber] === 'string' && row[columnNumber].includes('PSC')) {
+            tempObj['psoe'] = Number(row[columnNumber + 2])
+          }
+        }
+
+        byParty.push(tempObj)
+
+      }
+
+    })
+
+    console.log(byParty)
+  }
+  
+  const handleUploadedFile = file => {
+    handlePaginaPueblos(file.data)
+    handleMasVotados(file.data)
+    handleOrderedByParty(file.data)
+  }
+  
+  return (
+    <>
+    <h2 style={{ fontWeight: 100 }}>Elecciones locales 2023</h2>
+    <CSVReader onUploadAccepted={(file) => { handleUploadedFile(file) }}>
+    {({ getRootProps, acceptedFile, ProgressBar, getRemoveFileProps }) => (
+      <>
+      <div>
+      <button style={{ fontWeight: 100, minWidth: '33vw' }} type='button' {...getRootProps()}>Subir datos</button>
+      {/* <button style={{ fontWeight: 100 }} {...getRemoveFileProps()}>Limpiar memoria</button> */}
+      </div>
+      <ProgressBar style={{ marginBottom: '1rem' }} />
+      </>
+      )}
+      </CSVReader>
+
+      {
+        (paginaPueblosData?.length || masVotadosData?.length || dataByParty?.length) &&
+        <>
+        <hr></hr>
+        <h2 style={{ fontWeight: 100 }}>Resultados</h2>
+        </>
+      }
+      
+      {
+        paginaPueblosData?.length && 
+        <CSVDownloader type={Type.Button} filename={'28m-pagina-pueblos'} bom={true} config={{ delimiter: '\t', 'header': false }} data={paginaPueblosData}>
+        <span style={{ fontWeight: 400 }}>Página de pueblos</span>
+        </CSVDownloader>
+      }
+      
+      {
+        masVotadosData?.length && 
+        <CSVDownloader type={Type.Button} filename={'28m-mas-votados'} bom={true} config={{ delimiter: '\t', 'header': true }} data={masVotadosData}>
+        <span style={{ fontWeight: 400 }}>Más votados</span>
+        </CSVDownloader>
+      }
+      
+      </>
+      )
+    }
+    
+    export default App
+    
