63936820b32148be4362f0db909f28723b09a0a4
Juanjo <elquefaltaba@gmail.com>
2023-05-22 15:41:37 +0200

    changed implementation of pagina de pueblos to account for different use cases in Jaén and Almería

--- src/App.jsx
+++ src/App.jsx
@@ -12,28 +12,85 @@ function App() {
   const [paginaPueblosData, setPaginasPueblosData] = useState()
   const [masVotadosData, setMasVotadosData] = useState()
   const [dataByParty, setDataByParty] = useState()
+  const [checked, setChecked] = useState(false)
   
   const handlePaginaPueblos = data => {
-    const municipiosObjects = data.map(row => {
-      return {
-        municipio: row[6]?.trim(),
-        porcentajeVotantes: row[14] ? (Number(row[14]?.toString().replace(',','.')).toString().replace('.',',')) : null,
-        uno: row[23]?.trim(),
-        unoConcejales: Number(row[26]) !== 0 ? Number(row[26]) : null,
-        dos: row[28]?.trim(),
-        dosConcejales: Number(row[31]) !== 0 ? Number(row[31]) : null,
-        tres: row[33]?.trim(),
-        tresConcejales: Number(row[36]) !== 0 ? Number(row[31]) : null,
-        cuatro: row[38]?.trim(),
-        cuatroConcejales: Number(row[41]) !== 0 ? Number(row[41]) : null,
-        cinco: row[43]?.trim(),
-        cincoConcejales: Number(row[46]) !== 0 ? Number(row[46]) : null,
-        seis: row[48]?.trim(),
-        seisConcejales: Number(row[51]) !== 0 ? Number(row[51]) : null
+    // const municipiosObjects = data.map(row => {
+    //   return {
+    //     municipio: row[6]?.trim(),
+    //     porcentajeVotantes: row[14] ? (Number(row[14]?.toString().replace(',','.')).toString().replace('.',',')) : null,
+    //     uno: row[23]?.trim(),
+    //     unoConcejales: Number(row[26]) !== 0 ? Number(row[26]) : null,
+    //     dos: row[28]?.trim(),
+    //     dosConcejales: Number(row[31]) !== 0 ? Number(row[31]) : null,
+    //     tres: row[33]?.trim(),
+    //     tresConcejales: Number(row[36]) !== 0 ? Number(row[36]) : null,
+    //     cuatro: row[38]?.trim(),
+    //     cuatroConcejales: Number(row[41]) !== 0 ? Number(row[41]) : null,
+    //     cinco: row[43]?.trim(),
+    //     cincoConcejales: Number(row[46]) !== 0 ? Number(row[46]) : null,
+    //     seis: row[48]?.trim(),
+    //     seisConcejales: Number(row[51]) !== 0 ? Number(row[51]) : null
+    //   }
+    // })
+    
+    const municipiosObjects = []
+    
+    each(data, row => {
+      let tempMunicipioObject = {
+        municipio: row[6]?.trim()
       }
+      
+      if (!checked) {
+        tempMunicipioObject.porcentajeVotantes = row[14] ? (Number(row[14]?.toString().replace(',','.')).toString().replace('.',',')) : null
+      }
+      
+      tempMunicipioObject.uno = row[23]?.trim()
+      tempMunicipioObject.unoConcejales = Number(row[26]) !== 0 ? Number(row[26]) : null
+      
+      if (!checked) {
+        tempMunicipioObject.unoPorcentaje = Number(row[25]) !== 0 ? Number(row[26]) : null
+      }
+      
+      tempMunicipioObject.dos = row[28]?.trim()
+      tempMunicipioObject.dosConcejales = Number(row[31]) !== 0 ? Number(row[31]) : null
+      
+      if (!checked) {
+        tempMunicipioObject.dosPorcentaje = Number(row[30]) !== 0 ? Number(row[30]) : null
+      }
+      
+      tempMunicipioObject.tres = row[33]?.trim()
+      tempMunicipioObject.tresConcejales = Number(row[36]) !== 0 ? Number(row[36]) : null
+      
+      if (!checked) {
+        tempMunicipioObject.tresPorcentaje = Number(row[35]) !== 0 ? Number(row[35]) : null
+      }
+      
+      tempMunicipioObject.cuatro = row[38]?.trim()
+      tempMunicipioObject.cuatroConcejales = Number(row[41]) !== 0 ? Number(row[41]) : null
+      
+      if (!checked) {
+        tempMunicipioObject.cuatroPorcentaje = Number(row[40]) !== 0 ? Number(row[40]) : null
+      }
+      
+      tempMunicipioObject.cinco = row[43]?.trim()
+      tempMunicipioObject.cincoConcejales = Number(row[46]) !== 0 ? Number(row[46]) : null
+      
+      if (!checked) {
+        tempMunicipioObject.cincoPorcentaje = Number(row[45]) !== 0 ? Number(row[45]) : null
+      }
+      
+      if (!checked) {
+        tempMunicipioObject.seis = row[48]?.trim(),
+        tempMunicipioObject.seisConcejales = Number(row[51]) !== 0 ? Number(row[51]) : null
+        tempMunicipioObject.seisPorcentaje = Number(row[50]) !== 0 ? Number(row[50]) : null
+      }
+      
+      municipiosObjects.push(tempMunicipioObject)
+      
     })
     
-    // console.log(municipiosObjects)
+    console.log(municipiosObjects)
     
     setPaginasPueblosData(municipiosObjects)
   }
@@ -123,7 +180,7 @@ function App() {
       
     })
     
-    console.log(byParty)
+    // console.log(byParty)
     setDataByParty(byParty)
   }
   
@@ -133,12 +190,24 @@ function App() {
     handleOrderedByParty(file.data)
   }
   
+  const handleCheckedChange = () => {
+    setChecked(!checked)
+  }
+  
   return (
     <>
     <h2 style={{ fontWeight: 100 }}>Elecciones locales 2023</h2>
     <CSVReader onUploadAccepted={(file) => { handleUploadedFile(file) }}>
     {({ getRootProps, acceptedFile, ProgressBar, getRemoveFileProps }) => (
       <>
+      
+      <div style={{ marginBottom: '1rem' }}>
+      <label htmlFor="is-granada">
+      <input type="checkbox" name="is-granada" id="is-granada" checked={checked} onChange={handleCheckedChange} />
+      Datos de Granada
+      </label>
+      </div>
+      
       <div>
       <button style={{ fontWeight: 100, minWidth: '33vw' }} type='button' {...getRootProps()}>Subir datos</button>
       {/* <button style={{ fontWeight: 100 }} {...getRemoveFileProps()}>Limpiar memoria</button> */}
